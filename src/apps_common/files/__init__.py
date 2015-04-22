cb59b046b08e4ad84163ecac586ec99f6e43a890
pix666 <pix666@ya.ru>
2015-04-22 22:06:54 +0300

    clean styles

--- src/apps_common/files/__init__.py
+++ src/apps_common/files/__init__.py
@@ -2,23 +2,23 @@ from .models import PageFile
 
 """
     Модуль файлов на страницу.
-    
+
     Пример:
         models.py:
             from files import PageFile
-        
-            class CompanyFile(PageFile):
+
+            class ModuleFile(PageFile):
                 STORAGE_LOCATION = options.FILES_PATH
-                
-                company = models.ForeignKey(Company, verbose_name='Компания', related_name='files')
+
+                module = models.ForeignKey(Module, related_name='files')
 
                 def generate_filename(self, filename):
-                    return '%d/%s' % (self.company.pk, filename)
-        
+                    return '%d/%s' % (self.module.pk, filename)
+
         admin.py:
-            from .models import CompanyFile
+            from .models import ModuleFile
             from files.admin import PageFileInline
 
-            class CompanyFileInline(PageFileInline):
-                model = CompanyFile
+            class ModuleFileInline(PageFileInline):
+                model = ModuleFile
 """
