303a212619d77b4b7ffd588f68fbe6c9042edd11
pix <pix666@ya.ru>
2015-04-23 19:03:59 +0300

    video-bg

---
+++ src/static/js/video_bg.js
@@ -0,0 +1,86 @@
+(function($) {
+
+    /*
+         <div id="video" class="video-bg">
+             <div class="video-bg-wrapper">
+                <video preload="auto" src="{% static 'main/img/bay4.mp4' %}" autoplay="" loop=""></video>
+             </div>
+             ...
+         </div>
+     */
+
+    var blocks = [];
+
+    var refresh_video = function($block, settings) {
+        var $video = $block.find('video');
+        var win_height = $(window).outerHeight();
+
+        $block.height('');
+        var block_height = $block.outerHeight();
+        $block.outerHeight(Math.max(block_height, win_height));
+
+        $video.css({
+            height: '',
+            width: '',
+            marginLeft: '',
+            marginTop: ''
+        });
+        var video_height = $video.outerHeight();
+        block_height = $block.outerHeight();
+        if (video_height < block_height) {
+            $video.css({
+                height: block_height,
+                width: 'auto'
+            });
+
+            var video_width = $video.outerWidth();
+            var block_width = $block.outerWidth();
+            $video.css('marginLeft', Math.round((block_width - video_width) * settings.left));
+        } else if (settings.top) {
+            $video.css('marginTop', Math.round((block_height - video_height) * settings.top));
+        }
+    };
+
+
+    var refresh_all_videos = function() {
+        for (var i = 0, l = blocks.length; i < l; i++) {
+            var $block = blocks[i];
+            var settings = $block.data('video-bg-settings');
+
+            if (settings) {
+                refresh_video($block, settings);
+            }
+        }
+    };
+
+
+    $.fn.videoBackground = function(options) {
+        var settings = $.extend({
+            top: 0,
+            left: 0.5
+        }, options);
+
+        return $(this).each(function() {
+            var $block = $(this);
+            var $wrapper = $block.find('.video-bg-wrapper');
+            var $video = $wrapper.find('video');
+
+            $video.off('loadedmetadata.video-bg').on('loadedmetadata.video-bg', function() {
+                $(this).css('visibility', '');
+                refresh_video($block, settings);
+            });
+
+            refresh_video($block, settings);
+
+            blocks.push($block.data('video-bg-settings', settings));
+        });
+    };
+
+
+    $(window).on('resize.video-bg', $.rared(refresh_all_videos, 150));
+
+    $(document).ready(function () {
+        refresh_all_videos();
+    });
+
+})(jQuery);
