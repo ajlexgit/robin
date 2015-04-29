68eeb1faa419e3841187106d8ec448d7518dd0ee
pix <pix666@ya.ru>
2015-04-29 16:56:53 +0300

    grid-full-paddings + todo

--- src/manage.py
+++ src/manage.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+# -webkit-transform transition
 # юзать no_fontsize/default_fontsize; border-box
 # Сделать SHOW_VARIATION для StdImage + ссылку на исходник, если None
 # Решить проблему получения url исходника StdImage, когда файла нет
