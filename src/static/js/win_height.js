ea3d96df551e4a537193971da9d7b6e380acff75
pix <pix666@ya.ru>
2015-05-22 11:36:35 +0300

    win_height multiplier

--- src/static/js/win_height.js
+++ src/static/js/win_height.js
@@ -9,15 +9,16 @@
             rared.js
 
         Пример:
-            $.winHeight('.block');      - разовая установка высоты
-            $('.block').winHeight();    - обновляемая высота
+            $.winHeight('.block', 0.9);      - разовая установка высоты на 90% высоты окна
+            $('.block').winHeight(0.9);      - обновляемая высота d 90% высоты окна
 
     */
 
     var winBlocks = [];
     var $window = $(window);
 
-    $.winHeight = function($blocks) {
+    $.winHeight = function($blocks, multiplier) {
+        multiplier = multiplier || 1;
         var win_height = document.documentElement.clientHeight;
 
         if (!$blocks.jquery) {
@@ -27,26 +28,29 @@
         return $blocks.each(function () {
             var $block = $(this).height('auto');
             var block_height = $block.outerHeight();
-            $block.outerHeight(Math.max(block_height, win_height));
+            var final_height = Math.round(win_height * multiplier);
+            $block.outerHeight(Math.max(block_height, final_height));
         })
     };
 
     // Обновление высот блоков при ресайзе
     $window.on('resize.winHeight', $.rared(function() {
         $.each(winBlocks, function(index, winBlock) {
-            $.winHeight(winBlock.$block);
+            $.winHeight(winBlock.$block, winBlock.multiplier);
         });
     }, 50));
 
-    $.fn.winHeight = function() {
+    $.fn.winHeight = function(multiplier) {
         return this.each(function () {
             var $block = $(this);
 
             var winBlock = {
-                $block: $block
+                $block: $block,
+                multiplier: multiplier
             };
-            $.winHeight($block);
             winBlocks.push(winBlock);
+
+            $.winHeight($block, multiplier);
         });
     };
 
