1bbafba8c71ef08a7ee855509815a203f38b8b59
pix <pix666@ya.ru>
2015-06-03 14:42:10 +0300

    resetmigrations fix

--- src/apps_common/management/management/commands/resetmigrations.py
+++ src/apps_common/management/management/commands/resetmigrations.py
@@ -6,24 +6,37 @@ from django.core.management import BaseCommand, call_command
 
 
 class Command(BaseCommand):
-    
+
     def handle(self, *args, **options):
-        local_apps = tuple(
-            path for path in apps.get_app_paths()
-            if path.startswith(settings.BASE_DIR)
-        )
-        
+        local_apps = {
+            app.__name__.rsplit('.', 1)[0] : os.path.dirname(app.__file__) for app in apps.get_apps()
+            if app.__file__.startswith(settings.BASE_DIR)
+        }
+
+        if args:
+            ok = True
+            result_apps = {}
+            for arg in args:
+                if arg in local_apps:
+                    result_apps[arg] = local_apps[arg]
+                else:
+                    ok = False
+                    print('"%s" app not found' % arg)
+
+            if not ok:
+                return
+        else:
+            result_apps = local_apps
+
         # Удаление миграций
-        for path in local_apps:
+        for name, path in result_apps.items():
             migrations_dir = os.path.join(path, 'migrations')
             if os.path.isdir(migrations_dir):
                 shutil.rmtree(migrations_dir)
-        
+
         # Создание новых миграций
-        for path in local_apps:
-            appname = os.path.basename(path)
-            print('Create migrations for %s...' % appname)
-            call_command('makemigrations', appname, verbosity=0)
-        
+        for name, path in result_apps.items():
+            print('Create migrations for %s...' % name)
+            call_command('makemigrations', name, verbosity=0)
+
         call_command('migrate', verbosity=0)
-            
