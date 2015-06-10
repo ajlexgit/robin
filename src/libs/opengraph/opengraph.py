c28557029e833b973587f44e4148596eb35b7f81
pix <pix666@ya.ru>
2015-06-10 13:12:42 +0300

    fix opengraph

--- src/libs/opengraph/opengraph.py
+++ src/libs/opengraph/opengraph.py
@@ -3,17 +3,19 @@ from django.template import loader
 
 class Opengraph(dict):
     OG_PREFIXED = ('url', 'title', 'image', 'description', 'type')
-    
+
     def render(self):
         params = {
             'og:type': 'website',
         }
         for key, value in self.items():
-            if key in self.OG_PREFIXED:
-                params['og:%s' % key] = str(value).strip()
-            else:
-                params[key] = str(value).strip()
-        
+            value = str(value).strip()
+            if value:
+                if key in self.OG_PREFIXED:
+                    params['og:%s' % key] = value
+                else:
+                    params[key] = value
+
         return loader.render_to_string('opengraph/block.html', {
             'params': params,
         })
