1272f54f9fe9b73bce53193d77fdaad86222d2ca
pix <pix666@ya.ru>
2015-06-10 17:53:12 +0300

    fix

--- src/static/js/text_styles.js
+++ src/static/js/text_styles.js
@@ -4,8 +4,8 @@
         Требует:
             jquery.fitvids.js
 
-        Для автоматического ресайза Instagram нужно подключить скрипт:
-            <script async="" defer="" src="//platform.instagram.com/en_US/embeds.js"></script>
+        Для резиновости iframe интаграма нужно подключить скрипт:
+            <script src="//platform.instagram.com/en_US/embeds.js"></script>
     */
 
     $(document).ready(function() {
