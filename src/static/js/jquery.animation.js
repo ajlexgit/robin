398f1ddcad149c2e297a6aa6bfdd410dcc2e8698
pix <pix666@ya.ru>
2015-06-11 17:13:07 +0300

    fix animation.js

--- src/static/js/jquery.animation.js
+++ src/static/js/jquery.animation.js
@@ -1,8 +1,32 @@
-/**
-* Provides requestAnimationFrame in a cross browser way.
-* http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-*/
 (function($) {
+
+    /*
+        $.animation_frame
+            Обертка requestAnimationFrame для функций, работающих с CSS-анимацией.
+
+        $.animation
+            Кастомная JS-анимация. Главным образом предназначена для
+            анимации CSS3-свойств, которые не поддерживаются jQuery.
+
+            Есть возможность поставить анимацию на паузу.
+
+            Параметры:
+                duration: 1000                          - длительность анимации
+                delay: 20                               - частота фреймов анимации
+                easing: 'linear'                        - функция сглаживания
+                paused: false                           - создать приостановленной
+
+                init: $.noop                            - callback инициализации
+                step: function(eProgress)               - шаг анимации
+                start: $.noop                           - callback запуска анимации
+                stop: $.noop                            - callback остановки анимации
+                complete: $.noop                        - callback полного завершения анимации
+
+            Методы:
+                start()     - запуск анимации
+                stop()      - приостановить анимацию
+                reset()     - сбросить прогресс анимации
+    */
 
     var handler = window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
@@ -17,35 +41,101 @@
         return $.proxy(handler, window, callback, element)
     };
 
-    // Animate
-    $.animate = function(options) {
-        var settings = $.extend({
+
+    function Animation(options) {
+        var that = this;
+        that.paused = true;
+        that.settings = $.extend({
             duration: 1000,
+            delay: 20,
             easing: 'linear',
+            paused: false,
+
             init: $.noop,
             step: $.noop,
+            start: $.noop,
+            stop: $.noop,
             complete: $.noop
         }, options);
-        
-        settings.duration = Math.max(settings.duration || 0, 20);
-        settings.init.call(settings);
-        
-        var start = $.now();
-        var timer = setInterval(function() {
-            var progress = ($.now() - start) / settings.duration;
-            if (progress >= 1) {
-                progress = 1;
-                clearInterval(timer);
+
+        var applyProgress = function() {
+            // Применение шага анимации (для уже установленного that.progress)
+            var easeProgress = $.easing[that.settings.easing](that.progress);
+            that.settings.step.call(that, easeProgress);
+
+            if (that.progress === 1) {
+                that.stop();
+                that.settings.complete.call(that);
+            }
+        };
+
+        var timerHandle = function() {
+            // Применение шага анимации с вычислением that.progress
+            that.progress = ($.now() - that._start) / that.duration;
+            if (that.progress >= 1) {
+                that.progress = 1;
+                clearInterval(that._timer);
             }
 
-            var easeProgress = $.easing[settings.easing](progress);
-            settings.step.call(settings, easeProgress, progress);
-            
-            if (progress === 1) {
-                settings.complete.call(settings);
+            applyProgress();
+        };
+
+
+        that.start = function() {
+            // Запуск анимации
+            if (that.paused && (that.progress < 1)) {
+                that.paused = false;
+
+                if (that.progress) {
+                    that._start = $.now() - (that.progress * that.duration);
+                } else {
+                    that._start = $.now();
+                }
+                that._timer = setInterval(timerHandle, that.settings.delay);
+
+                that.settings.start.call(that);
             }
-        }, 20);
-        return timer;
+            return that
+        };
+
+        that.stop = function(jumpToEnd) {
+            // Остановка анимации
+            if (!that.paused) {
+                clearInterval(that._timer);
+                that.paused = true;
+
+                if (jumpToEnd) {
+                    that.progress = 1;
+                    applyProgress();
+                } else {
+                    that.settings.stop.call(that);
+                }
+            }
+            return that
+        };
+
+        that.reset = function() {
+            // Сброс анимации
+            that.progress = 0;
+            if (!that.paused) {
+                that._start = $.now();
+            }
+            that.duration = Math.max(that.settings.duration || 0, 20);
+            return that
+        };
+
+        // Инициализация
+        that.reset();
+        that.settings.init.call(that);
+
+        if (!that.settings.paused) {
+            that.start();
+        }
+    }
+
+    // Animate
+    $.animate = function(options) {
+        return new Animation(options);
     };
-    
+
 })(jQuery);
