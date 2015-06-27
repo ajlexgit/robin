e5b542a3945c8a12262eb0c1f4cb02bab4fd5f23
pix666 <pix666@ya.ru>
2015-06-27 21:05:19 +0300

    fix help

--- src/libs/hits/__init__.py
+++ src/libs/hits/__init__.py
@@ -1,20 +1,19 @@
 """
     Модуль счетчика по дням для экземпляров моделей.
-    
+
     Примеры:
         entry = Post.objects.get(pk=1)
-        
-        # Получить сумму значений счетчика за всё время
-        total_hits = Hits.get(entry, type='my action')
-        
-        # Увеличить счетчик (для текущего дня) на 1
-        Hits.increment(entry, type='other action')
-        
-        # Получить значение счетчика за вчерашний день
-        since = datetime.now() - timedelta(1)
-        to = since
-        yesterday_hits = Hits.get(entry, type='my action', since=since, to=to)
-        
-        # Удалить все значения счетчика
-        Hits.clear(entry, type='my action')
+
+        # Увеличить счетчик типа shows (для текущего дня) на 1
+        Hits.increment(entry, type='shows')
+
+        # Получить сумму значений счетчика shows за всё время
+        total_hits = Hits.get(entry, type='shows')
+
+        # Получить значение счетчика click за вчерашний день
+        yesterday = datetime.now() - timedelta(1)
+        yesterday_hits = Hits.get(entry, type='click', since=yesterday, to=yesterday)
+
+        # Удалить все значения счетчика click
+        Hits.clear(entry, type='click')
 """
