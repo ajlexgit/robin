71b2fce96aa55148787fdb0c55f8fe87ca2245a2
pix666 <pix666@ya.ru>
2015-06-27 21:01:27 +0300

    REMOVE size_field and resolution

--- src/libs/size_field/__init__.py
+++
@@ -1,8 +0,0 @@
-from .models import SizeField
-
-"""
-    Поле для ввода размера чего-либо в виде двух цифр.
-    
-    Пример:
-        size = SizeField('размер', min_value='50x50', max_value=(100, 150), default='100x100')
-"""
