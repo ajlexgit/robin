ee83c36128ef788adc6a854a52b95b1e86deba09
pix666 <pix666@ya.ru>
2015-08-17 00:06:26 +0300

    slider

--- src/static/js/slider.js
+++ src/static/js/slider.js
@@ -48,9 +48,18 @@
             this.$slides = this.createSlides(this.opts.slideItems);
             this.$slides.addClass(this.opts.slideClass);
 
-            // активация слайдов
-            this.$currentSlide = this.getStartSlide();
-            this.$currentSlide.addClass(this.opts.slideActiveClass);
+            // активация первого слайда
+            this.activateSlide(this.getStartSlide());
+
+            // Установка стиля left слайдам
+            var that = this;
+            this.$slides.each(function() {
+                if (this == that.$currentSlide.get(0)) {
+                    $(this).css('left', 0);
+                } else {
+                    $(this).css('left', '100%');
+                }
+            });
 
             // --- стрелки ---
             if (this.opts.controls) {
@@ -73,6 +82,9 @@
 
                 itemSelector: 'img',
                 slideItems: 2,
+                loop: true,
+
+                speed: 500,
 
                 controls: true,
                 controlsParent: null
@@ -118,6 +130,13 @@
             return this.$slides.first();
         };
 
+        // Метод, возвращающий начальный активный слайд
+        Slider.prototype.activateSlide = function($slide) {
+            this.$slides.removeClass(this.opts.slideActiveClass);
+            $slide.addClass(this.opts.slideActiveClass);
+            this.$currentSlide = $slide;
+        };
+
         // Метод, возвращающий контейнер, куда будут добавлены стрелки
         Slider.prototype.getControlsContainer = function() {
             if (this.opts.controlsParent) {
@@ -130,14 +149,133 @@
 
         // Метод, добавляющий стрелки
         Slider.prototype.createControls = function($controlsParent) {
-            console.log($controlsParent);
             var $left = $('<div>').addClass('arrow arrow-left').appendTo($controlsParent);
             var $right = $('<div>').addClass('arrow arrow-right').appendTo($controlsParent);
+
+            var that = this;
+            $left.on('click', function() {
+                that.slideLeft();
+            });
+            $right.on('click', function() {
+                that.slideRight();
+            });
 
             return {
                 left: $left,
                 right: $right
             };
+        };
+
+        // Метод, возвращающий следующий слайд
+        Slider.prototype.getNextSlide = function($slide) {
+            var index = this.$slides.index($slide);
+            if (++index < this.$slides.length) {
+                return this.$slides.eq(index);
+            }
+
+            if (this.opts.loop) {
+                return this.$slides.eq(0);
+            }
+        };
+
+        // Метод, возвращающий предыдущий слайд
+        Slider.prototype.getPreviousSlide = function($slide) {
+            var index = this.$slides.index($slide);
+            if (--index >= 0) {
+                return this.$slides.eq(index);
+            }
+
+            if (this.opts.loop) {
+                return this.$slides.eq(this.$slides.length - 1);
+            }
+        };
+
+        // Скролл к следующему слайду
+        Slider.prototype.slideRight = function() {
+            var $curr = this.$currentSlide;
+            if ($curr._animation) {
+                return
+            }
+
+            var $next = this.getNextSlide($curr);
+            if (!$next || !$next.length) {
+                return
+            }
+
+            $curr._animation = $.animate({
+                duration: this.opts.speed,
+                init: function() {
+                    this.initial = parseInt($curr.get(0).style.left);
+                    this.diff = -100 - this.initial;
+                },
+                step: function(eProgress) {
+                    $curr.css('left', this.initial + this.diff * eProgress + '%');
+                },
+                complete: function() {
+                    $curr._animation = null;
+                }
+            });
+
+            this.activateSlide($next);
+            $next.css('left', '100%');
+
+            $next._animation = $.animate({
+                duration: this.opts.speed,
+                init: function() {
+                    this.initial = parseInt($next.get(0).style.left);
+                    this.diff = -this.initial;
+                },
+                step: function(eProgress) {
+                    $next.css('left', this.initial + this.diff * eProgress + '%');
+                },
+                complete: function() {
+                    $next._animation = null;
+                }
+            });
+        };
+
+        // Скролл к предыдущему слайду
+        Slider.prototype.slideLeft = function() {
+            var $curr = this.$currentSlide;
+            if ($curr._animation) {
+                return
+            }
+
+            var $prev = this.getPreviousSlide($curr);
+            if (!$prev || !$prev.length) {
+                return
+            }
+
+            $curr._animation = $.animate({
+                duration: this.opts.speed,
+                init: function() {
+                    this.initial = parseInt($curr.get(0).style.left);
+                    this.diff = 100 - this.initial;
+                },
+                step: function(eProgress) {
+                    $curr.css('left', this.initial + this.diff * eProgress + '%');
+                },
+                complete: function() {
+                    $curr._animation = null;
+                }
+            });
+
+            this.activateSlide($prev);
+            $prev.css('left', '-100%');
+
+            $prev._animation = $.animate({
+                duration: this.opts.speed,
+                init: function() {
+                    this.initial = parseInt($prev.get(0).style.left);
+                    this.diff = -this.initial;
+                },
+                step: function(eProgress) {
+                    $prev.css('left', this.initial + this.diff * eProgress + '%');
+                },
+                complete: function() {
+                    $prev._animation = null;
+                }
+            });
         };
 
         return Slider;
