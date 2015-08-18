d13b17b697f9592cda7a54075172e07775061c5f
pix <pix666@ya.ru>
2015-08-18 09:42:40 +0400

    new animation

--- src/static/js/jquery.animation.js
+++ src/static/js/jquery.animation.js
@@ -41,101 +41,102 @@
         return $.proxy(handler, window, callback, element)
     };
 
-
-    function Animation(options) {
-        var that = this;
-        var startTime, timer;
-        that.paused = true;
-        that.settings = $.extend({
-            duration: 1000,
-            delay: 20,
-            easing: 'linear',
-            paused: false,
-
-            init: $.noop,
-            step: $.noop,
-            start: $.noop,
-            stop: $.noop,
-            complete: $.noop
-        }, options);
-
-        var applyProgress = function () {
-            // Применение шага анимации (для уже установленного that.progress)
-            var easeProgress = $.easing[that.settings.easing](that.progress);
-            that.settings.step.call(that, easeProgress);
-
-            if (that.progress === 1) {
-                that.stop();
-                that.settings.complete.call(that);
-            }
-        };
-
-        var timerHandle = function () {
-            // Применение шага анимации с вычислением that.progress
-            that.progress = ($.now() - startTime) / that.duration;
-            if (that.progress >= 1) {
-                that.progress = 1;
-                clearInterval(timer);
-            }
-
-            applyProgress();
-        };
-
-
-        that.start = function () {
-            // Запуск анимации
-            if (that.paused && (that.progress < 1)) {
-                that.paused = false;
-
-                if (that.progress) {
-                    startTime = $.now() - (that.progress * that.duration);
-                } else {
-                    startTime = $.now();
-                }
-                timer = setInterval(timerHandle, that.settings.delay);
-
-                that.settings.start.call(that);
-            }
-            return that
-        };
-
-        that.stop = function (jumpToEnd) {
-            // Остановка анимации
-            if (!that.paused) {
-                clearInterval(timer);
-                that.paused = true;
-
-                if (jumpToEnd) {
-                    that.progress = 1;
-                    applyProgress();
-                } else {
-                    that.settings.stop.call(that);
-                }
-            }
-            return that
-        };
-
-        that.reset = function () {
-            // Сброс анимации
-            that.progress = 0;
-            if (!that.paused) {
-                startTime = $.now();
-            }
-            that.duration = Math.max(that.settings.duration || 0, 20);
-            return that
-        };
-
-        // Инициализация
-        that.reset();
-        that.settings.init.call(that);
-
-        if (!that.settings.paused) {
-            that.start();
-        }
-    }
-
-    $.animate = function (options) {
+    $.animate = function(options) {
         return new Animation(options);
     };
+
+    var Animation = (function() {
+        var Animation = function(settings) {
+            this.opts = $.extend({
+                duration: 1000,
+                delay: 20,
+                easing: 'linear',
+                paused: false,
+
+                init: $.noop,
+                step: $.noop,
+                start: $.noop,
+                stop: $.noop,
+                complete: $.noop
+            }, settings);
+
+            this.reset();
+            this.paused = true;
+            this.opts.init.call(this);
+
+            if (!this.opts.paused) {
+                this.start();
+            }
+        };
+
+        // Сброс анимации
+        Animation.prototype.reset = function() {
+            this.progress = 0;
+            if (!this.paused) {
+                this._startTime = $.now();
+            }
+            this._duration = Math.max(this.opts.duration || 0, 20);
+            return this
+        };
+
+        // Запуск анимации
+        Animation.prototype.start = function() {
+            if (this.paused && (this.progress < 1)) {
+                this.paused = false;
+
+                if (this.progress) {
+                    this._startTime = $.now() - (this.progress * this._duration);
+                } else {
+                    this._startTime = $.now();
+                }
+
+                var timerHandle = $.proxy(this._timerHandle, this);
+                this._timer = setInterval(timerHandle, this.opts.delay);
+
+                this.opts.start.call(this);
+            }
+            return this
+        };
+
+        // Остановка анимации
+        Animation.prototype.stop = function(jumpToEnd) {
+            if (!this.paused) {
+                clearInterval(this._timer);
+                this.paused = true;
+
+                if (jumpToEnd) {
+                    this.progress = 1;
+                    this._applyProgress();
+                } else {
+                    this.opts.stop.call(this);
+                }
+            }
+            return this
+        };
+
+        // Применение шага анимации с вычислением that.progress
+        Animation.prototype._timerHandle = function() {
+            this.progress = ($.now() - this._startTime) / this._duration;
+            if (this.progress >= 1) {
+                this.progress = 1;
+                clearInterval(this._timer);
+            }
+
+            this._applyProgress();
+        };
+
+        // Применение шага анимации (для уже установленного that.progress)
+        Animation.prototype._applyProgress = function() {
+            var easeProgress = $.easing[this.opts.easing](this.progress);
+            this.opts.step.call(this, easeProgress);
+
+            if (this.progress === 1) {
+                this.stop();
+                this.opts.complete.call(this);
+            }
+        };
+
+        return Animation;
+    })();
 
 })(jQuery);
