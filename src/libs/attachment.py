454abdbb3f526e8b1efb2dad0c47e9ff3aa2c413
pix <pix666@ya.ru>
2015-08-19 18:54:39 +0400

    lil fix

--- src/libs/attachment.py
+++ src/libs/attachment.py
@@ -30,10 +30,7 @@ def AttachmentResponse(request, file_path, original_filename=None):
         response['Content-Encoding'] = encoding
 
     # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
-    if 'WebKit' in request.META['HTTP_USER_AGENT']:
-        # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
-        filename_header = 'filename=%s' % original_filename
-    elif 'MSIE' in request.META['HTTP_USER_AGENT']:
+    if 'MSIE' in request.META['HTTP_USER_AGENT']:
         # IE does not support internationalized filename at all.
         # It can only recognize internationalized URL, so we do the trick via routing rules.
         filename_header = ''
