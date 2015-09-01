ffa003296320e3ea46bb5e3b072c03d12f8758c5
pix <pix666@ya.ru>
2015-09-01 18:56:34 +0400

    picturefill

--- src/settings/common.py
+++ src/settings/common.py
@@ -179,6 +179,7 @@ PIPELINE_JS = {
             'js/jquery-2.1.4.min.js',
             'js/jquery-ui.min.js',
             'js/jquery.cookie.js',
+            'js/jquery.picturefill.min.js',
         ),
         'output_filename': 'js/head_core.js',
     },
