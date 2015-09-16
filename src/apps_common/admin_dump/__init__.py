bf49c9c7a041d4dfd718e9e0a6a566e9173b4609
pix <pix666@ya.ru>
2015-09-16 09:53:31 +0400

    pretty fixes

--- src/apps_common/admin_dump/__init__.py
+++ src/apps_common/admin_dump/__init__.py
@@ -1,6 +1,9 @@
 """
     Модуль бэкапов данных.
 
+    Зависит от:
+        libs.download
+
     Установка:
         settings.py:
 
@@ -19,7 +22,8 @@
                 },
                 ...
             }
-            
+
             BACKUP_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'backup'))
 """
+
 default_app_config = 'admin_dump.apps.Config'
