affacf708e4f1750293bac0ce30285809d7d4278
pix <pix666@ya.ru>
2015-09-18 10:34:53 +0400

    messages.sh

--- src/messages.sh
+++ src/messages.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
-pushd $1 > /dev/null
-case "$2" in
+directory=$1
+command=$2
+args=${*:3}
+
+if [[ -z "$directory" ]]
+then
+    echo "empty directory"
+    exit
+fi
+
+pushd $directory > /dev/null
+case "$command" in
     -c | --compile)
         django-admin compilemessages
         ;;
-    -m | --make)
-        django-admin makemessages --no-obsolete ${@:3}
-        ;;
     *)
-        django-admin makemessages --no-obsolete ${@:2}
+        mkdir -p locale
+        args="$2 $args"
+        args="$(echo -e "${args}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+        
+        if [[ -z "$args" ]]
+        then
+            args="-l ru"
+        fi
+        
+        django-admin makemessages --no-obsolete $args
         ;;
 esac
 popd > /dev/null
