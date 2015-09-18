954680333de71baeb294274f8de5ec0b576c1cd1
pix666 <pix666@ya.ru>
2015-09-18 22:36:44 +0400

    upgrade restore_backup.sh

--- src/restore_backup.sh
+++ src/restore_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-rm -r ../media
+# Example: ./restore_backup.sh ../backup/18_09_2015.zip
+rm -rf ../media
 unzip -o $1 media/* -d ../
 unzip $1 dump.json
 python3 manage.py loaddata dump.json
