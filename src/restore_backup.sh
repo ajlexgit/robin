7fba01701c2864b4f748ea052f59b1f9366c4c31
pix <pix666@ya.ru>
2015-09-22 17:46:42 +0400

    fix + todo

--- src/restore_backup.sh
+++ src/restore_backup.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 # Example: ./restore_backup.sh ../backup/18_09_2015.zip
-rm -rf ../media
-unzip -o $1 media/* -d ../
-unzip $1 dump.json
+
+archive=$1
+directory=$2
+
+if [[ -z "$directory" ]]
+then
+    directory=..
+fi
+
+rm -rf $directory/media
+unzip -o $archive media/* -d $directory/
+unzip $archive dump.json
 python3 manage.py loaddata dump.json
 rm dump.json
