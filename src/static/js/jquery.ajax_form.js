acddaac92be1e18dcf1f4f53222690d26957e136
pix <pix666@ya.ru>
2015-10-08 19:31:09 +0400

    data for ajax_form

--- src/static/js/jquery.ajax_form.js
+++ src/static/js/jquery.ajax_form.js
@@ -6,8 +6,35 @@
         Автоматически блокируется попытка одновременной
         отправки нескольких экземпляров формы.
 
-        Параметры аналогичны параметрам $.ajax;
+        URL и метод (GET/POST) по умолчанию берутся из аттрибутов формы.
+
+        Параметры аналогичны параметрам $.ajax, за исключением того,
+        что добавлен метод modifyData, принимающий объект данных формы,
+        который может его модифицировать.
      */
+
+    var re_split_name = /^([^\[\]]+)(\[.*])?$/;     // "x[][name][5]"  =>  [..., "x", "[][5]"]
+    var re_brackets = /\[.*?]/g;    // "[][name][5]"  => ["[]", "[name]", "[5]"]
+
+    var buildFormData = function($form) {
+        var data_array = $form.serializeArray();
+        var form_data = {};
+        for (var i = 0, l = data_array; i < l; i++) {
+            var form_field = data_array[i];
+
+            var arr = re_split_name.exec(form_field.name);
+            if (!arr) {
+                console.error('buildFormData: bad field name "' + form_field.name + '"');
+                continue
+            }
+
+            var name = arr[1];
+            var params = arr[2];
+
+            form_data[name] = form_field.value;
+        }
+    };
+
 
     var onSubmit = function(settings) {
         var $form = $(this);
@@ -15,9 +42,11 @@
             return false;
         }
 
-        var default_data = $form.serialize();
+        var form_data = buildFormData($form);
+        var data = settings.modifyData(form_data);
+
         $.ajax($.extend({
-            data: default_data
+            data: data
         }, settings));
 
         return false;
@@ -40,7 +69,10 @@
             var settings = $.extend(true, {
                 url: $form.attr('action'),
                 type: $form.attr('method'),
-                dataType: 'json'
+                dataType: 'json',
+                modifyData: function(data) {
+                    return data
+                }
             }, options, {
                 beforeSend: function() {
                     this.$form = $form;
