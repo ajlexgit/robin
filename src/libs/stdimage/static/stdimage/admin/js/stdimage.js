c3d27cb807a185fa66772694361e6024ae94a1df
pix <pix666@ya.ru>
2015-10-26 14:10:08 +0400

    fix crop

--- src/libs/stdimage/static/stdimage/admin/js/stdimage.js
+++ src/libs/stdimage/static/stdimage/admin/js/stdimage.js
@@ -1,10 +1,8 @@
 (function($) {
 
-    var positionImage = function($preview, source, coords) {
-        var $image = $preview.find('img');
-
+    var positionImage = function($field, $image, source, coords) {
         // target
-        var target_size = $preview.data('size') || '';
+        var target_size = $field.data('size') || '';
         if (!$.isArray(target_size)) {
             target_size = String(target_size).split('x');
         }
@@ -13,7 +11,7 @@
         }).filter($.isNumeric).slice(0, 2);
 
         // offset
-        var offset = $preview.data('offset') || '';
+        var offset = $field.data('offset') || '';
         if (!$.isArray(offset)) {
             offset = String(offset).split('x');
         }
@@ -22,7 +20,7 @@
         }).filter($.isNumeric).slice(0, 2);
 
         // center
-        var center = $preview.data('center') || '';
+        var center = $field.data('center') || '';
         if (!$.isArray(center)) {
             center = String(center).split('x');
         }
@@ -31,7 +29,7 @@
         }).filter($.isNumeric).slice(0, 2);
 
         // background
-        var background = $preview.data('background') || '255,255,255,0';
+        var background = $field.data('background') || '255,255,255,0';
         if (!$.isArray(background)) {
             background = String(background).split(',');
         }
@@ -39,10 +37,10 @@
             return parseInt(item);
         }).filter($.isNumeric).slice(0, 4);
 
-        var crop = parseInt($preview.data('crop'));
-        var stretch = parseInt($preview.data('stretch'));
-        var max_width = parseInt($preview.data('max_width'));
-        var max_height = parseInt($preview.data('max_height'));
+        var crop = parseInt($field.data('crop'));
+        var stretch = parseInt($field.data('stretch'));
+        var max_width = parseInt($field.data('max_width'));
+        var max_height = parseInt($field.data('max_height'));
 
         var canvas = $.previewCanvas({
             source: source,
@@ -64,18 +62,17 @@
     $(document).on('change', '.stdimage .uploader', function() {
         var $input = $(this),
             $field = $input.closest('.stdimage'),
+            field_data = $field.data(),
             $preview = $field.find('.item-preview').addClass('preloader').empty(),
-            preview_data = $preview.data(),
             $crop_btn_wrapper = $field.find('.crop-btn-wrapper');
+
+        // сброс обрезки
+        $crop_btn_wrapper.find('input').val('');
 
         $.fileReaderDeferred($input.prop('files').item(0)).done(function(src) {
             $.loadImageDeferred(src).done(function(img) {
                 var $image = $('<img/>');
-                $crop_btn_wrapper
-                    .show()
-                    .find('button')
-                    .removeData('crop')
-                    .removeAttr('data-crop');
+                $crop_btn_wrapper.show();
 
                 $preview
                     .show()
@@ -86,14 +83,14 @@
                     })
                     .append($image);
 
-                preview_data.source = src;
-                $image.attr('src', preview_data.source);
+                field_data.source = src;
+                $image.attr('src', field_data.source);
 
-                positionImage($preview, img);
+                positionImage($field, $image, img);
             }).fail(function(reason) {
                 $input.val('');
                 $preview.removeClass('preloader').hide();
-                preview_data.source = '';
+                field_data.source = '';
 
                 if (reason == 'Not image') {
                     alert(gettext('Файл не является изображением'));
@@ -104,7 +101,7 @@
         }).fail(function(reason) {
             $input.val('');
             $preview.removeClass('preloader').hide();
-            preview_data.source = '';
+            field_data.source = '';
 
             if (reason != 'Not a file') {
                 alert(reason);
@@ -115,41 +112,43 @@
 
     $(document).cropdialog('click.cropdialog', '.stdimage .crop-btn-wrapper button', {
         image_url: function($element) {
-            var $field = $element.closest('.stdimage'),
-                $preview = $field.find('.item-preview');
-            return $preview.data('source');
+            var $field = $element.closest('.stdimage');
+            return $field.data('source');
         },
         min_size: function($element) {
-            return $element.data('min_dimensions');
+            var $field = $element.closest('.stdimage');
+            return $field.data('min_dimensions');
         },
         max_size: function($element) {
-            return $element.data('max_dimensions');
+            var $field = $element.closest('.stdimage');
+            return $field.data('max_dimensions');
         },
         aspect: function($element) {
-            return $element.data('aspects');
+            var $field = $element.closest('.stdimage');
+            return $field.data('aspects');
         },
         crop_position: function($element) {
-            return $element.data('crop') || '';
+            var $wrapper = $element.closest('.crop-btn-wrapper');
+            return $wrapper.find('input').val();
         },
         onCrop: function($element, coords) {
             var $field = $element.closest('.stdimage');
-            var $preview = $field.find('.item-preview');
-            var $image = $preview.find('img');
+            var $wrapper = $element.closest('.crop-btn-wrapper');
+            var $image = $field.find('.item-preview').find('img');
 
             // Загружаем исходник и позиционируем его
-            var source_url = $preview.data('source');
+            var source_url = $field.data('source');
             if (source_url.substr(0, 4) != 'data') {
                 source_url += '?_=' + Math.random().toString().substr(2);
             }
             $.loadImageDeferred(source_url).done(function(img) {
                 $image.attr('src', img.src);
-                positionImage($preview, img, coords);
+                positionImage($field, $image, img, coords);
             });
 
             // Записываем координаты в форму
             var coords_text = coords.join(':');
-            $field.find('input[name$="-croparea"]').val(coords_text);
-            $element.data('crop', coords_text);
+            $wrapper.find('input').val(coords_text);
         }
     });
 
