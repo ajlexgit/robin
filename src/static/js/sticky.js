e6c852f6d7c36d35f0e7ec57297d9b39afe80e5e
pix <pix666@ya.ru>
2015-10-29 12:52:38 +0400

    sticky strategies

--- src/static/js/sticky.js
+++ src/static/js/sticky.js
@@ -8,6 +8,7 @@
             jquery.utils.js
 
         Параметры:
+            strategy:         - метод перемещения блока ("margin" или "fixed")
             topOffset         - расстояние от верха окна до ползающего блока
             bottomOffset      - расстояние от низа родительского блока
                                 до ползающего блока
@@ -15,6 +16,10 @@
 
     var $window = $(window);
     var stickies = [];
+
+    var capitalize = function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    };
 
     window.Sticky = (function() {
         var Sticky = function(block, options) {
@@ -40,6 +45,7 @@
 
         Sticky.prototype.getDefaultOpts = function() {
             return {
+                strategy: 'margin',     // margin / fixed
                 topOffset: 50,
                 bottomOffset: 50,
                 minEnableWidth: 768
@@ -56,6 +62,9 @@
                 this.enabled = true;
             }
 
+            if (this.opts.strategy == 'fixed') {
+                this.updateWidth();
+            }
             this.process();
         };
 
@@ -74,11 +83,85 @@
             })
         };
 
+
+        /*
+            Запоминаем ширину блока (для strategy = fixed)
+         */
+        Sticky.prototype.updateWidth = function() {
+            var initial_css = this.$block.get(0).style.cssText;
+            this.$block.get(0).style.cssText = '';
+
+            this._width = this.$block.width();
+            this.$block.get(0).style.cssText = initial_css;
+
+            return this._width;
+        };
+
+
+        /*
+            Обработка скролла
+         */
         Sticky.prototype.process = function(win_scroll) {
             if (!this.enabled) {
                 return
             }
 
+            var methodName = '_process' + capitalize(this.opts.strategy);
+            if (methodName in this) {
+                this[methodName].call(this, win_scroll);
+            }
+        };
+
+
+        /*
+            Обработка скролла при стратегии fixed
+         */
+        Sticky.prototype._processFixed = function(win_scroll) {
+            var container_top = this.$container.offset().top + (parseInt(this.$container.css('padding-top')) || 0);
+            var block_height = this.$block.outerHeight();
+            var container_height = this.$container.height();
+            var scrollFrom = container_top - this.opts.topOffset;
+            var scrollTo = scrollFrom + container_height - block_height - this.opts.bottomOffset;
+
+            if (win_scroll < scrollFrom) {
+                if (this._state != 'top') {
+                    this._state = 'top';
+
+                    this.$block.css({
+                        position: '',
+                        top: '',
+                        bottom: '',
+                        width: ''
+                    });
+                }
+            } else if (win_scroll > scrollTo) {
+                if (this._state != 'bottom') {
+                    this._state = 'bottom';
+                    this.$block.css({
+                        position: 'absolute',
+                        top: '',
+                        bottom: this.opts.bottomOffset,
+                        width: ''
+                    });
+                }
+            } else {
+                if (this._state != 'middle') {
+                    this._state = 'middle';
+
+                    this.$block.css({
+                        position: 'fixed',
+                        top: this.opts.topOffset,
+                        bottom: '',
+                        width: this._width
+                    });
+                }
+            }
+        };
+
+        /*
+            Обработка скролла при стратегии margin
+         */
+        Sticky.prototype._processMargin = function(win_scroll) {
             var container_top = this.$container.offset().top + (parseInt(this.$container.css('padding-top')) || 0);
             var block_height = this.$block.outerHeight();
             var container_height = this.$container.height();
