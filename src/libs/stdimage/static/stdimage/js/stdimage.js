2526553e4ff0d05fd7638cd737cd573e303d24e9
pix666 <pix666@ya.ru>
2015-11-07 21:08:57 +0400

    new class for stcImage

--- src/libs/stdimage/static/stdimage/js/stdimage.js
+++ src/libs/stdimage/static/stdimage/js/stdimage.js
@@ -1,114 +1,21 @@
 (function($) {
 
-    window.StdImage = (function() {
-        var StdImage = function() {};
-
-        StdImage.create = function(root, options) {
-            var self = new StdImage();
-
-            self.$root = $.findFirstElement(root);
-            if (!self.$root.length) {
+    window.StdImage = Class(null, function(cls, superclass) {
+        cls.init = function(root, options) {
+            this.$root = $(root).first();
+            if (!this.$root.length) {
                 console.log('StdImage can\'t find root element');
-                return
+                return false;
             }
 
-            self.$input = $.findFirstElement('input[type="file"]', self.$root);
-            if (!self.$input.length) {
+            this.$input = this.$root.find('input[type="file"]');
+            if (!this.$input.length) {
                 console.log('StdImage can\'t find file element');
-                return
+                return false;
             }
 
-            self.opts = $.extend(self.getDefaultOpts(), options);
-
-            // превью
-            self.$previews = $.findFirstElement(self.opts.previewsSelector, self.$root);
-            self.$old_preview = $.findFirstElement(self.opts.oldPreviewSelector, self.$root);
-            self.$new_preview = $.findFirstElement(self.opts.newPreviewSelector, self.$root);
-
-            self._wasEmpty = self.$root.hasClass(self.opts.emptyClass) || !self.$old_preview.length;
-
-            // EVENTS
-            var drag_counter = 0;
-            self.$root.off('.stdimage').on('dragenter.stdimage', function() {
-                drag_counter++;
-                if (drag_counter == 1) {
-                    $(this).addClass(self.opts.dragOverClass);
-                }
-                return false;
-            }).on('dragleave.stdimage', function() {
-                drag_counter--;
-                if (drag_counter === 0) {
-                    $(this).removeClass(self.opts.dragOverClass);
-                }
-                return false;
-            }).on('drop.stdimage', function() {
-                drag_counter = 0;
-                var $this = $(this);
-                setTimeout(function() {
-                    $this.removeClass(self.opts.dragOverClass);
-                }, 0);
-            });
-
-            // Изменение файла
-            self.$input.off('.stdimage').on('change.stdimage', function() {
-                var $input = $(this);
-
-                var file = $input.prop('files');
-                file = file && file.length && file[0];
-                if (!file) {
-                    self.reset();
-                    return;
-                }
-
-                self.$root.addClass(self.opts.preloaderClass);
-
-                $.fileReaderDeferred(file).done(function(src) {
-                    $.loadImageDeferred(src).done(function(img) {
-                        src = null;
-
-                        // размер превью
-                        var size = self.$root.data('size').toString()
-                            .split('x').map(function(item) {
-                                return parseInt(item)
-                            }).filter(Boolean);
-                        if (size.length != 2) {
-                            size = [self.$previews.width(), self.$previews.height()];
-                        }
-
-                        var final_canvas = $.previewCanvas({
-                            source: img,
-                            width: size[0],
-                            height: size[1],
-                            crop: true,
-                            stretch: false
-                        });
-
-
-                        self.$root.removeClass(self.opts.emptyClass);
-                        self.$root.removeClass(self.opts.preloaderClass);
-
-                        self.$old_preview.hide();
-                        self.$new_preview.attr('src', final_canvas.toDataURL()).show();
-                    }).fail(function() {
-                        self.reset();
-                        self.$root.removeClass(self.opts.preloaderClass);
-                    })
-                }).fail(function() {
-                    self.reset();
-                    self.$root.removeClass(self.opts.preloaderClass);
-                });
-            });
-
-            self.$root.data('stdimage', self);
-
-            return self;
-        };
-
-        /*
-            Настройки по умолчанию
-         */
-        StdImage.prototype.getDefaultOpts = function() {
-            return {
+            // настройки
+            this.opts = $.extend({
                 previewsSelector: '.previews',
                 oldPreviewSelector: '.old-preview',
                 newPreviewSelector: '.new-preview',
@@ -116,13 +23,103 @@
                 emptyClass: 'empty',
                 dragOverClass: 'dragover',
                 preloaderClass: 'preloader'
+            }, options);
+
+            // превью
+            this.$previews = this.$root.find(this.opts.previewsSelector);
+            this.$old_preview = this.$root.find(this.opts.oldPreviewSelector);
+            this.$new_preview = this.$root.find(this.opts.newPreviewSelector);
+
+            this.$root.data('stdimage', this);
+
+            // значение не установлено изначально
+            this._wasEmpty = this.$root.hasClass(this.opts.emptyClass) || !this.$old_preview.length;
+
+            var that = this;
+            var drag_counter = 0;
+
+            // вешаем класс на перетаскивании
+            this.$root.on('dragenter.stdimage', function() {
+                drag_counter++;
+                if (drag_counter == 1) {
+                    $(this).addClass(that.opts.dragOverClass);
+                }
+                return false;
+            }).on('dragleave.stdimage', function() {
+                drag_counter--;
+                if (drag_counter === 0) {
+                    $(this).removeClass(that.opts.dragOverClass);
+                }
+                return false;
+            }).on('drop.stdimage', function() {
+                drag_counter = 0;
+                var $this = $(this);
+                setTimeout(function() {
+                    $this.removeClass(that.opts.dragOverClass);
+                }, 0);
+            });
+
+            // Изменение файла
+            this.$input.on('change.stdimage', function() {
+                that.changeHandler($(this));
+            });
+        };
+
+        /*
+            Событие изменения файла
+         */
+        cls.prototype.changeHandler = function($input) {
+            var file = $input.prop('files');
+            file = file && file.length && file[0];
+            if (!file) {
+                this.reset();
+                return;
             }
+
+            this.$root.addClass(this.opts.preloaderClass);
+
+            var that = this;
+            $.fileReaderDeferred(file).done(function(src) {
+                $.loadImageDeferred(src).done(function(img) {
+                    src = null;
+
+                    // размер превью
+                    var size = that.$root.data('size').toString()
+                        .split('x').map(function(item) {
+                            return parseInt(item)
+                        }).filter(Boolean);
+                    if (size.length != 2) {
+                        size = [that.$previews.width(), that.$previews.height()];
+                    }
+
+                    // создаем превью
+                    var final_canvas = $.previewCanvas({
+                        source: img,
+                        width: size[0],
+                        height: size[1],
+                        crop: true,
+                        stretch: false
+                    });
+
+                    that.$root.removeClass(that.opts.emptyClass);
+                    that.$root.removeClass(that.opts.preloaderClass);
+
+                    that.$old_preview.hide();
+                    that.$new_preview.attr('src', final_canvas.toDataURL()).show();
+                }).fail(function() {
+                    that.reset();
+                    that.$root.removeClass(that.opts.preloaderClass);
+                })
+            }).fail(function() {
+                that.reset();
+                that.$root.removeClass(that.opts.preloaderClass);
+            });
         };
 
         /*
             Сброс на первоначальное превью, если оно было
          */
-        StdImage.prototype.reset = function() {
+        cls.prototype.reset = function() {
             this.$new_preview.removeAttr('src').hide();
             if (this._wasEmpty) {
                 this.$root.addClass(this.opts.emptyClass);
@@ -130,9 +127,7 @@
                 this.$old_preview.show();
             }
         };
-
-        return StdImage;
-    })();
+    });
 
 
     $(document).ready(function() {
