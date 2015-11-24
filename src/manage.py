e002e9f5cd6a93a646e0ae05ba94a95a41bfefbf
pix <pix666@ya.ru>
2015-11-24 12:42:01 +0400

    fix

--- src/manage.py
+++ src/manage.py
@@ -2,6 +2,8 @@ import os
 import sys
 
 
+# default_font -> font_default
+# default_fontsize -> fontsize_default
 # JSON.parse в try catch
 # AJAX-авторизация на странице регистрации
 # new JS popups + show/hide return popup
