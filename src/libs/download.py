da253e161f10a8ea78cb5d223d5907fc4c71e7c8
pix <pix666@ya.ru>
2015-11-30 18:01:47 +0400

    fix

--- src/libs/download.py
+++ src/libs/download.py
@@ -4,7 +4,7 @@ from urllib.parse import quote
 from django.http import HttpResponse
 
 
-def AttachmentResponse(request, file_path, original_filename=None):
+def AttachmentResponse(request, file, original_filename=None):
     """
         Response, заставляющий браузер скачать файл.
 
@@ -12,19 +12,21 @@ def AttachmentResponse(request, file_path, original_filename=None):
             # views.py
             def download(request, file_id):
                 page_file = get_object_or_404(PageFile, pk=file_id)
-                return AttachmentResponse(request, page_file.file.path)
+                return AttachmentResponse(request, page_file.file)
     """
-    with open(file_path, 'rb') as fp:
-        response = HttpResponse(fp.read())
+    if isinstance(file, str):
+        file = open(file, 'rb')
+
+    response = HttpResponse(file)
 
     if original_filename is None:
-        original_filename = os.path.basename(file_path)
+        original_filename = os.path.basename(file.name)
 
     type_name, encoding = mimetypes.guess_type(original_filename)
     if type_name is None:
         type_name = 'application/octet-stream'
     response['Content-Type'] = type_name
-    response['Content-Length'] = str(os.stat(file_path).st_size)
+    response['Content-Length'] = str(len(response.content))
     if encoding is not None:
         response['Content-Encoding'] = encoding
 
