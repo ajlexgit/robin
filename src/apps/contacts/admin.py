beb37178fb036c10c3d48f07dfdafb2d4b75a7a5
pix <pix666@ya.ru>
2016-01-25 16:47:16 +0400

    fix permissions

--- src/apps/contacts/admin.py
+++ src/apps/contacts/admin.py
@@ -98,3 +98,12 @@ class ContactBlockAdmin(ModelAdminMixin, admin.ModelAdmin):
     suit_form_tabs = (
         ('general', _('General')),
     )
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
