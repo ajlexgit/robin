68f0da824df880af4b9726217ef487dcd19bbf03
pix <pix666@ya.ru>
2016-02-12 10:44:00 +0400

    fix backup help

--- src/apps_common/backups/__init__.py
+++ src/apps_common/backups/__init__.py
@@ -24,11 +24,6 @@
 
             BACKUP_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'backup'))
 
-        urls.py:
-            ...
-            url(r'^dladmin/dump/', include('backups.admin_urls', namespace='backups')),
-            ...
-
 """
 
 default_app_config = 'backups.apps.Config'
