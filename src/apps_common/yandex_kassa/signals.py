3e2deb9890df2734673d0f3f14e6f1236a3e5484
mike <pix666@ya.ru>
2016-02-13 15:37:18 +0400

    RM yandex kassa

--- src/apps_common/yandex_kassa/signals.py
+++
@@ -1,4 +0,0 @@
-from django.dispatch import Signal
-
-payment_process = Signal(providing_args=['instance', 'site'])
-payment_completed = Signal(providing_args=['instance', 'site'])
--- src/apps_common/yandex_kassa/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .views import NoticeFormView
-from .views import CheckOrderFormView
-
-
-urlpatterns = [
-    url(r'^check/$', CheckOrderFormView.as_view(), name='yandex_money_check'),
-    url(r'^aviso/$', NoticeFormView.as_view(), name='yandex_money_notice'),
-]
