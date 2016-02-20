ff39db39fdb1d92a53244c57b115a3033571b5c4
pix <pix666@ya.ru>
2016-02-20 18:37:00 +0400

    cookies

--- src/manage.py
+++ src/manage.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+# JS dates
 # admin move inlines animation
 # facebook like button: https://developers.facebook.com/docs/plugins/like-button?locale=ru_RU
 # права на поля и блоки
