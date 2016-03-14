1f16097341388d47501b8da9696fc4e8c37eecdc
pix <pix666@ya.ru>
2016-03-14 15:52:37 +0400

    todo

--- src/manage.py
+++ src/manage.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 # crop each variation
-# filereader big files
 # js templates
 # facebook like button: https://developers.facebook.com/docs/plugins/like-button?locale=ru_RU
 # text-ellipsis: https://css-tricks.com/line-clampin/
