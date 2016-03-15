6a1e4e23292376621e41b6749d799a22b9e10bde
pix <pix666@ya.ru>
2016-03-15 17:39:40 +0400

    fix

--- src/settings/common.py
+++ src/settings/common.py
@@ -447,7 +447,7 @@ TEMPLATES = [
             'context_processors': (
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.i18n',
-                'django.core.context_processors.request',
+                'django.template.context_processors.request',
             ),
             'loaders': (
                 ('django.template.loaders.cached.Loader', [
