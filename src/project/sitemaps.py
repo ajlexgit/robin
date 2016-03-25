3b8ab248a03762dfdb9261b51bf6165ab161c163
pix <pix666@ya.ru>
2016-03-25 10:17:14 +0400

    rename sitemap dict

--- src/project/sitemaps.py
+++ src/project/sitemaps.py
@@ -1,12 +1,12 @@
 from django.contrib.sitemaps import GenericSitemap
 from main.models import MainPageConfig
 
-mainpage_dict = {
+mainpage = {
     'queryset': MainPageConfig.objects.all(),
     'date_field': 'updated',
 }
 
 
 site_sitemaps = {
-    'main': GenericSitemap(mainpage_dict, changefreq='weekly', priority=0.5),
+    'main': GenericSitemap(mainpage, changefreq='weekly', priority=0.5),
 }
