cf14f21b103e6a214a35bd7e4b81c12b4f380509
pix <pix666@ya.ru>
2016-03-29 14:32:23 +0400

    radioSelect

--- src/libs/widgets.py
+++ src/libs/widgets.py
@@ -1,5 +1,6 @@
-from datetime import time
 from django import forms
+from datetime import time
+from django.forms import widgets
 from django.utils.encoding import force_str
 from django.utils.html import format_html, smart_urlquote
 from django.forms.utils import flatatt, to_current_timezone
@@ -79,3 +80,27 @@ class TimeWidget(forms.widgets.Input):
         if isinstance(value, time):
             value = value.strftime('%H:%M')
         return super().render(name, value, attrs)
+
+
+class RadioChoiceInput(widgets.RadioChoiceInput):
+    def render(self, name=None, value=None, attrs=None, choices=()):
+        if self.id_for_label:
+            label_for = format_html(' for="{}"', self.id_for_label)
+        else:
+            label_for = ''
+        attrs = dict(self.attrs, **attrs) if attrs else self.attrs
+        return format_html(
+            '{} <label{}> {}</label>', self.tag(attrs), label_for, self.choice_label
+        )
+
+
+class RadioFieldRenderer(widgets.RadioFieldRenderer):
+    choice_input_class = RadioChoiceInput
+
+
+class RadioSelect(forms.RadioSelect):
+    """
+        Аналог RadioSelect, но вместо того, чтобы <input> был внутри <label>,
+        помещает <input> перед <label> (для стилизации).
+    """
+    renderer = RadioFieldRenderer
