0c591d6c7c563800cfd1e5dc77ba17248763fa0f
pix <pix666@ya.ru>
2016-05-17 18:58:49 +0400

    test

--- src/apps/contacts/views_ajax.py
+++ src/apps/contacts/views_ajax.py
@@ -17,7 +17,7 @@ class ContactView(AjaxViewMixin, View):
                 'form': form,
             }),
         })
-        response['Cache-Control'] = 'max-age=%d' % 24 * 3600
+        response['cache-control'] = 'max-age=%d' % 24 * 3600
         return response
 
     def post(self, request):
