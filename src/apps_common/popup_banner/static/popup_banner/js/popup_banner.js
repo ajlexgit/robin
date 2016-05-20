d251cdb6652ea2e594d66fa62e4d5a70b4e611f8
pix <pix666@ya.ru>
2016-05-20 18:05:51 +0400

    popup banner

--- src/apps_common/popup_banner/static/popup_banner/js/popup_banner.js
+++ src/apps_common/popup_banner/static/popup_banner/js/popup_banner.js
@@ -14,6 +14,23 @@
         Показ окна баннера
      */
     window.bannerPopup = function() {
+        var cookie_name = 'banner_' + window.js_storage.popup_banner_id;
+        var show_type = window.js_storage.popup_show_type;
+        var was_shown = $.cookie(cookie_name);
+
+        $.removeCookie(cookie_name);
+        if (show_type == SHOW_ONCE_SESSION) {
+            $.cookie(cookie_name, 1);
+        } else if (show_type == SHOW_ONCE) {
+            $.cookie(cookie_name, 1, {
+                expires: 180
+            });
+        }
+
+        if (was_shown && (show_type != SHOW_ALWAYS)) {
+            return
+        }
+        
         return $.ajax({
             url: window.js_storage.ajax_popup_banner,
             type: 'GET',
