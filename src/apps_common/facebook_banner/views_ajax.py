f10df788fa0246350e8516949c5a440d0e0910c1
pix <pix666@ya.ru>
2016-06-02 12:36:14 +0400

    fix pagespeed

--- src/apps_common/facebook_banner/views_ajax.py
+++ src/apps_common/facebook_banner/views_ajax.py
@@ -1,8 +1,13 @@
+from django.views.decorators.cache import cache_control
 from django.views.generic.base import View
 from libs.views_ajax import AjaxViewMixin
 
 
 class BannerView(AjaxViewMixin, View):
+    def get_handler(self, request):
+        handler = super().get_handler(request)
+        return cache_control(private=True, max_age=7*24*3600)(handler)
+
     def get(self, request):
         return self.json_response({
             'html': self.render_to_string('facebook_banner/banner.html'),
