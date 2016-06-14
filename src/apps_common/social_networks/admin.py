496917fa085a630d539df285e58f48b9212fbc60
pix <pix666@ya.ru>
2016-06-14 18:41:50 +0400

    share mixin

--- src/apps_common/social_networks/admin.py
+++ src/apps_common/social_networks/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from django.conf.urls import url
+from django.core.urlresolvers import reverse
+from django.http.response import Http404, JsonResponse
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.staticfiles.storage import staticfiles_storage
 from project.admin import ModelAdminMixin
 from .models import SocialPost
-from .forms import SocialPostForm
+from .forms import SocialPostForm, AutpostForm
 from . import conf
 
+AUTOPOST_FORM_PREFIX = 'autopost'
 SPRITE_ICONS = (
     conf.NETWORK_TWITTER,
     conf.NETWORK_FACEBOOK,
@@ -42,6 +46,12 @@ class SocialPostAdmin(ModelAdminMixin, admin.ModelAdmin):
         ('general', _('General')),
     )
 
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
     def suit_cell_attributes(self, obj, column):
         """ Классы для ячеек списка """
         default = super().suit_cell_attributes(obj, column)
@@ -77,3 +87,87 @@ class SocialPostAdmin(ModelAdminMixin, admin.ModelAdmin):
     def action_unschedule_posts(self, request, queryset):
         queryset.update(scheduled=False)
     action_unschedule_posts.short_description = _('Unschedule %(verbose_name_plural)s to be published')
+
+
+class AutoPostMixin(ModelAdminMixin):
+    change_form_template = 'social_networks/admin/change_form.html'
+
+    class Media:
+        js = (
+            'social_networks/admin/js/autopost.js',
+        )
+        css = {
+            'all': (
+                'social_networks/admin/css/autopost.css',
+            )
+        }
+
+    def has_autopost_permissions(self, request):
+        """ Проверка, есть ли права на редактирование автопостинга """
+        return request.user.has_perm('social_networks.change_socialpost')
+
+    def get_autopost_text(self, obj):
+        return obj.note
+
+    def get_autopost_url(self, obj):
+        return obj.get_absolute_url()
+
+    def get_autopost_form(self, request, obj):
+        if request.method == 'POST':
+            return AutpostForm(
+                request.POST,
+                request.FILES,
+                initial={
+                    'text': self.get_autopost_text(obj),
+                },
+                prefix=AUTOPOST_FORM_PREFIX
+            )
+        else:
+            return AutpostForm(
+                initial={
+                    'text': self.get_autopost_text(obj),
+                },
+                prefix=AUTOPOST_FORM_PREFIX
+            )
+
+    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
+        if not add:
+            info = self.model._meta.app_label, self.model._meta.model_name
+            context.update({
+                'has_share_permission': self.has_autopost_permissions(request),
+                'share_form': self.get_autopost_form(request, obj),
+                'share_form_url': reverse('admin:%s_%s_share' % info, args=(obj.pk,)),
+            })
+        return super().render_change_form(request, context, add, change, form_url, obj)
+
+    def get_urls(self):
+        urls = super().get_urls()
+
+        info = self.model._meta.app_label, self.model._meta.model_name
+        submit_urls = [
+            url(r'^(\d+)/share/$', self.admin_site.admin_view(self.submit_view), name='%s_%s_share' % info),
+        ]
+        return submit_urls + urls
+
+    def submit_view(self, request, object_id):
+        try:
+            obj = self.model._default_manager.get(pk=object_id)
+        except self.model.DoesNotExist:
+            raise Http404
+
+        form = self.get_autopost_form(request, obj)
+        if form.is_valid():
+            text = form.cleaned_data.get('text')
+            networks = form.cleaned_data.get('networks')
+            for network in networks:
+                SocialPost.objects.create(
+                    network=network,
+                    url=request.build_absolute_uri(self.get_autopost_url(obj)),
+                    text=text,
+                )
+
+            return JsonResponse({})
+        else:
+            return JsonResponse({
+                'errors': form.errors
+            }, status=400)
--- src/apps_common/social_networks/forms.py
+++ src/apps_common/social_networks/forms.py
@@ -1,4 +1,3 @@
-from collections import Iterable
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from .models import SocialPost
@@ -17,3 +16,26 @@ class SocialPostForm(forms.ModelForm):
         model = SocialPost
         fields = '__all__'
 
+
+class AutpostForm(forms.Form):
+    networks = forms.MultipleChoiceField(
+        required=True,
+        label=_('Social networks'),
+        choices=conf.ALLOWED_NETWORKS,
+        initial=SocialPost._meta.get_field('network').default,
+        widget=forms.CheckboxSelectMultiple,
+        error_messages={
+            'required': _('Please select at least one social network'),
+        }
+    )
+
+    text = forms.CharField(
+        required=True,
+        widget=forms.Textarea(attrs={
+            'maxlength': 1024,
+            'rows': 3,
+        }),
+        error_messages={
+            'required': _('Please enter text'),
+        }
+    )
