75cd02993945ae198582faa8f4b5a2af3dda4db6
mike <pix666@ya.ru>
2016-06-20 22:05:59 +0400

    untest

--- src/libs/cache/middleware.py
+++ src/libs/cache/middleware.py
@@ -33,10 +33,7 @@ class SCCMiddleware:
             return response
 
         # Если не HTML-страница - выходим
-        content_type = response['content-type']
-        is_html = 'text/html' in content_type
-        is_json = 'application/json' in content_type
-        if not is_html and not is_json:
+        if 'text/html' not in response['content-type']:
             return response
 
         # Если нет юзера - выходим
