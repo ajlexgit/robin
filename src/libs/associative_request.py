59e2fcf3b8b94489599d3a2258e862a712d44845
pix <pix666@ya.ru>
2016-07-05 16:49:24 +0400

    fix

--- src/libs/associative_request.py
+++ src/libs/associative_request.py
@@ -3,15 +3,15 @@ from collections import Iterable
 
 def _process(result, key, data):
     if isinstance(data, str):
-        result[key] = data
+        result.append((key, data))
     elif isinstance(data, dict):
         for index, inner_data in data.items():
             _process(result, '{}[{}]'.format(key, index), inner_data)
     elif isinstance(data, Iterable):
         for index, inner_data in enumerate(data):
-            _process(result, '{}[{}]'.format(key, index), inner_data)
+            _process(result, '{}[]'.format(key, index), inner_data)
     else:
-        result[key] = data
+        result.append((key, data))
 
 
 def associative(data):
@@ -20,12 +20,12 @@ def associative(data):
 
         Пример:
             > associative({'plain': 1, 'list': [1,2], 'dict': {'a':1, 'b':2}})
-            {'plain': 1, 'list[0]': 1, 'list[1]': 2, 'dict[a]': 1, 'dict[b]': 2}
+            [('plain', 1), ('list[]', 1), ('list[]', 2), ('dict[a]', 1), ('dict[b]', 2)]
     """
     if not data:
         return {}
 
-    result = {}
+    result = []
     for key, value in data.items():
         _process(result, key, value)
     return result
