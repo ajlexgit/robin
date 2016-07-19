781065b3358e9d52de27942f931adabfac986632
mike <pix666@ya.ru>
2016-07-19 19:43:21 +0400

    rm shop url

--- src/project/urls.py
+++ src/project/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     url(r'^500/$', 'django.views.defaults.server_error'),
 
     url(r'^blog/', include('blog.urls', namespace='blog')),
-    url(r'^shop/', include('shop.urls', namespace='shop')),
     url(r'^contact/', include('contacts.urls', namespace='contacts')),
 
     url(r'^admin/', include(honeypot_site.urls)),
