3c5968a0aa1260925c1e463c93d23277b356e9d4
mike <pix666@ya.ru>
2016-08-21 16:43:52 +0400

    ашч

--- src/manage.py
+++ src/manage.py
@@ -1,20 +1,16 @@
 import os
 import sys
 
-# std_page
 # pm delete_app
 # Blocks kwars on ajax
 # Share: выводить картинку и дать возможность изменить
 # Mobile tables
 # Images: alt, title, file name
 # Schema.org: per page + get data from models
-# Mailer antispam, proves
 # Admin: функция "скопировать из"
 # crop each variation
 # Кнопка подтверждения заказа в админке
 # Редактирование продуктов магазина в админке
-
-# Comments: есть возможность написать коммент к удаленному комменту. Как быть?
 # Logging, checks, tests
 
 if __name__ == "__main__":
