33142a3728cc954c1951a73e7cb16bf3d77f224a
pix <pix666@ya.ru>
2016-11-22 15:29:24 +0400

    shortdate

--- src/libs/templatetags/templatetags/dates.py
+++ src/libs/templatetags/templatetags/dates.py
@@ -1,37 +1,26 @@
-import re
-from django.conf import settings
-from django.template import Library, defaultfilters
 from django.utils import timezone
-from django.utils.translation import ugettext as _
+from django.utils.formats import date_format
+from django.utils.translation import ugettext as _, ungettext_lazy
 
 register = Library()
-
-year_re = re.compile('[\s\.]+(Y|y)')
 
 
 @register.filter(expects_localtime=True)
 def shortdate(value):
-    """
-        Выводит дату и время с учетом разницы во времени:
-
-        сегодня, 21:00
-        вчера, 21:00
-        16 февраля, 21:00       # текущий год
-        16 февраля 2014, 21:00  # другой год
-    """
-    now = timezone.localtime(timezone.now())
+    now = timezone.template_localtime(timezone.now())
     diff = now - value
 
     if diff < timezone.timedelta():
-        return defaultfilters.date(value)
-    elif diff < timezone.timedelta(1):
-        return '%s, %s' % (_('today'), defaultfilters.time(value))
-    elif diff < timezone.timedelta(2):
-        return '%s, %s' % (_('yesterday'), defaultfilters.time(value))
+        return date_format(value, "DATE_FORMAT")
+    elif diff.days < 1:
+        return _('today')
+    elif diff.days < 2:
+        return _('yesterday')
+    elif diff.days < 30:
+        return ungettext_lazy('%d day ago', '%d days ago') % diff.days
     else:
-        date_format = None
-        if now.year == value.year:
-            date_format = year_re.sub('', settings.DATE_FORMAT)
-        date = defaultfilters.date(value, date_format)
-        time = defaultfilters.time(value)
-        return '%s, %s' % (date, time)
+        months = diff.days // 30
+        if months <= 6:
+            return ungettext_lazy('a month ago', '%d months ago') % months
+        else:
+            return date_format(value, "DATE_FORMAT")
