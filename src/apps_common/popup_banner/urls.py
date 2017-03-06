a3c736e0ae3fb8f154b4d2290aa233672f2440ba
pix <pix666@ya.ru>
2017-03-06 21:42:27 +0400

    popup_banner archieved + rm cacheblock

--- src/apps_common/popup_banner/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views_ajax
-
-
-urlpatterns = [
-    url(r'^ajax/$', views_ajax.BannerView.as_view(), name='ajax_popup_banner'),
-]
--- src/libs/cacheblock/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views_ajax
-
-
-urlpatterns = [
-    url(r'^ajax/$', views_ajax.get_cached, name='ajax'),
-]
