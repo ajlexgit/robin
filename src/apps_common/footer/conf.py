0b494fa0777299d010743061d13915852e102b3f
pix <pix666@ya.ru>
2017-04-17 13:40:44 +0400

    dl link

---
+++ src/apps_common/footer/conf.py
@@ -0,0 +1,34 @@
+DL_LINKS = {
+    'home': (
+        {
+            'url': 'https://directlinedev.com/',
+            'title': 'Web Design Company',
+        },
+        {
+            'url': 'https://directlinedev.com/',
+            'title': 'Web Design',
+        },
+    ),
+    'services': (
+        {
+            'url': 'https://directlinedev.com/web-design/',
+            'title': 'Web Design Services',
+        },
+        {
+            'url': 'https://directlinedev.com/website-design/',
+            'title': 'Website Design Services',
+        },
+        {
+            'url': 'https://directlinedev.com/web-development/',
+            'title': 'Web Development',
+        },
+        {
+            'url': 'https://directlinedev.com/search-engine-optimization/',
+            'title': 'Search Engine Optimization',
+        },
+        {
+            'url': 'https://directlinedev.com/design-logo/',
+            'title': 'Logo Design',
+        },
+    )
+}
--- src/apps_common/footer/templatetags/footer.py
+++ src/apps_common/footer/templatetags/footer.py
@@ -1,4 +1,7 @@
+import binascii
 from django.template import Library, loader
+from django.contrib.sites.shortcuts import get_current_site
+from .. import conf
 
 register = Library()
 
@@ -7,5 +10,26 @@ register = Library()
 def footer(context, template='footer/footer.html'):
     """ Футер """
     return loader.render_to_string(template, {
-        
+
     }, request=context.get('request'))
+
+
+@register.simple_tag(takes_context=True)
+def dl_link(context, template='footer/dl_link.html'):
+    request = context.get('request')
+    if not request:
+        return ''
+
+    site = get_current_site(request)
+    website = site.domain
+    page_info = request.path_info
+
+    set_crc = binascii.crc32(('set:%s:%s' % (website, page_info)).encode())
+    set_names = tuple(sorted(conf.DL_LINKS.keys()))
+    url_set = conf.DL_LINKS[set_names[set_crc % len(set_names)]]
+
+    url_crc = binascii.crc32(('url:%s:%s' % (website, page_info)).encode())
+    records = tuple(sorted(url_set, key=lambda x: x['url']))
+    record = records[url_crc % len(url_set)]
+
+    return loader.render_to_string(template, record, request=context.get('request'))
