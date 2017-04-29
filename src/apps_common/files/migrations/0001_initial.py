8ecbedde06d71da4c3294288e3d45c510c55e923
pix <pix666@ya.ru>
2017-04-30 01:28:55 +0400

    fix

--- src/apps_common/files/migrations/0001_initial.py
+++ src/apps_common/files/migrations/0001_initial.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import libs.storages.media_storage
-import libs.file_field
+import libs.file_field.fields
 import files.models
 
 
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('object_id', models.PositiveIntegerField()),
-                ('file', libs.file_field.FileField(storage=libs.storages.media_storage.MediaStorage('files'), max_length=150, verbose_name='file')),
+                ('file', libs.file_field.fields.FileField(storage=libs.storages.media_storage.MediaStorage('files'), max_length=150, verbose_name='file')),
                 ('name', models.CharField(help_text='If you leave it empty the file name will be used', max_length=150, verbose_name='name', blank=True)),
                 ('set_name', models.CharField(max_length=32, default='default', verbose_name='set name')),
                 ('sort_order', models.PositiveIntegerField(verbose_name='sort order')),
