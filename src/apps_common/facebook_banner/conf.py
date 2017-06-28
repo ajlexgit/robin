a20527ace650bd34541c5e7c8de6a1dcd04bdbe7
pix <pix666@ya.ru>
2017-06-28 09:27:34 +0400

    fb app_id

--- src/apps_common/facebook_banner/conf.py
+++ src/apps_common/facebook_banner/conf.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
 # https://developers.facebook.com/apps/?action=create
-FACEBOOK_APP_ID = getattr(settings, 'FACEBOOK_APP_ID', getattr(settings, 'SOCIAL_AUTH_FACEBOOK_KEY', ''))
+FACEBOOK_BANNER_APP_ID = getattr(settings, 'FACEBOOK_BANNER_APP_ID', '')
 
 POPUP_TIMEOUT = 4000
