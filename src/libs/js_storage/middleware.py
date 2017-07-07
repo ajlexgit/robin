fc3084ca737b1ebf7f631b8d7b99a510e428f974
pix <pix666@ya.ru>
2017-07-07 12:01:10 +0400

    fix cart

--- src/libs/js_storage/middleware.py
+++ src/libs/js_storage/middleware.py
@@ -7,5 +7,5 @@ class JSStorageMiddleware:
     def process_request(request):
         request.js_storage = JS_STORAGE.copy()
         request.js_storage.update(
-            cookie_domain=settings.DOMAIN,
+            cookie_domain=settings.SESSION_COOKIE_DOMAIN,
         )
